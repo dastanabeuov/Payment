d9683c90e656e11f4eee36e9df4791fc63e2d2a7
ADK <web.dev.adk@gmail.com>
2022-07-25 16:29:18 +0600

    Added and edit some basic style(slim views / logo image / links).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'notice'
+      'alert-success'
+    when 'alert'
+      'alert-warning'
+    when 'error'
+      'alert-danger'
+    end
+  end
 end
